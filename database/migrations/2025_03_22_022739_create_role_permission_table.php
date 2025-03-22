68d39781120faa13396c21030f15835e88b1bc06
teddynanta <teddy.nanta99@gmail.com>
2025-03-22 09:48:57 +0700

    clean up mess

--- database/migrations/2025_03_22_022739_create_role_permission_table.php
+++ database/migrations/2025_03_22_022739_create_role_permission_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+        Schema::create('role_permission', function (Blueprint $table) {
+            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('permission_id')->constrained()->cascadeOnDelete();
+            $table->primary(['role_id', 'permission_id']);
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('role_permission');
     }
 };
